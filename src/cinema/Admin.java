c2e296726b0ddaf703705df8780b7a808d942538
mentesess <mentesess@gmail.com>
2014-07-10 15:02:40 +0300

    getEmptySeats method is added
    Session sınıfının içerisine seansdaki boş koltukları getiren bir method
    yazdık

--- src/cinema/Admin.java
+++ src/cinema/Admin.java
@@ -11,13 +11,21 @@ public class Admin {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		
-		Hall hall = new Hall(1, 3, 4, "ErolEgemen");
-		Session movie = new Session("Kaybedenler Kulubu",hall, Hours.EIGHTEEN);
-		new Ticket(3, 3,movie);
-		new Ticket(3, 4,movie);
+
+		boolean[][] emptySeat;
+		Hall hall = new Hall(1, 4, 4, "ErolEgemen");
+		Session movie = new Session("Kaybedenler Kulubu", hall, Hours.EIGHTEEN);
+		new Ticket(3, 3, movie);
+		new Ticket(3, 4, movie);
+		emptySeat = movie.getEmptySeats();
+		for (int i = 0; i < emptySeat.length; i++) {
+			for (int j = 0; j < emptySeat[i].length; j++) {
+				System.out.print(emptySeat[i][j]+" ");
+			}
+			System.out.println();
+		}
 		System.out.println(movie);
-		
+
 	}
 
 }
--- src/cinema/model/Session.java
+++ src/cinema/model/Session.java
@@ -1,5 +1,7 @@
 package cinema.model;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.HashMap;
 
 import cinema.exceptions.SameTicketException;
@@ -9,58 +11,77 @@ public class Session {
 	private Hall hall;
 	private Hours hour;
 	private String movieName;
-	private HashMap<Integer,Ticket> tickets;
+	private HashMap<Integer, Ticket> tickets;
 	private int counter;
-	
-	
-	
+
 	public Hours getHour() {
 		return hour;
 	}
+
 	public String getMovieName() {
 		return movieName;
 	}
-	public Session(String movieName,Hall hall,Hours hour) {
+
+	public Session(String movieName, Hall hall, Hours hour) {
 		super();
-		this.hall= hall;
+		this.hall = hall;
 		this.tickets = new HashMap<>();
 		counter = 0;
 		this.movieName = movieName;
 		this.hour = hour;
 		this.hall = hall;
 	}
-	
-	public boolean addTicket(Ticket newTicket){
-		if(tickets.containsKey(newTicket.getRows()* this.getHall().getCols()+newTicket.getCols())){
+
+	public boolean addTicket(Ticket newTicket) {
+		if (tickets.containsKey(newTicket.getRows() * this.getHall().getCols()
+				+ newTicket.getCols())) {
 			return false;
-		}
-		else{
-		tickets.put(newTicket.getRows()* this.getHall().getCols()+newTicket.getCols(), newTicket);
-		counter++;
-		return true;
+		} else {
+			tickets.put(newTicket.getRows() * this.getHall().getCols()
+					+ newTicket.getCols(), newTicket);
+			counter++;
+			return true;
 		}
 	}
+
 	public int getCounter() {
 		return counter;
 	}
-	public void print(){
-		for(int i = 0; i< hall.getRows();i++){
-			for(int j = 0; j <hall.getCols(); j++){
-	//			if(tickets.get(i))
+
+	public void print() {
+		for (int i = 0; i < hall.getRows(); i++) {
+			for (int j = 0; j < hall.getCols(); j++) {
+				// if(tickets.get(i))
 			}
 		}
 	}
+
 	public Hall getHall() {
 		return hall;
 	}
+
 	public HashMap<Integer, Ticket> getTickets() {
 		return tickets;
 	}
+
 	@Override
 	public String toString() {
 		return "Session [hall=" + hall + ", hour=" + hour + ", movieName="
-				+ movieName + ", tickets=" + tickets + ", Tickets count=" + counter
-				+ "]";
+				+ movieName + ", tickets=" + tickets + ", Tickets count="
+				+ counter + "]";
 	}
-	
+
+	public boolean[][] getEmptySeats() {
+		boolean[][] emptySeats = new boolean[hall.getRows()][hall.getCols()];
+		for (int i = 0; i < emptySeats.length; i++) {
+			for (int j = 0; j < emptySeats[i].length; j++) {
+				if (tickets.containsKey((i+1) * emptySeats[i].length + j+1)) {
+					emptySeats[i][j] = true;
+				} else {
+					emptySeats[i][j] = false;
+				}
+			}
+		}
+		return emptySeats;
+	}
 }
